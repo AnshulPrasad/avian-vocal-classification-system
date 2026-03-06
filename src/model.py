53d9fdc8ec8ca311e81f94113944792dcad7ea42
Anshul Prasad <anshulprasad5440.com>
2026-03-06 11:33:49 +0530

    shift method to class

--- src/model.py
+++ src/model.py
@@ -6,10 +6,13 @@
 import torchvision.models as models
 import torch.nn as nn
 
-def build_model(num_classes: int):
-    model = models.efficientnet_b0(weights='IMAGENET1K_V1')
-    model.classifier[1] = nn.Linear(
-        model.classifier[1].in_features,
-        num_classes
-    )
-    return model
+class Model:
+    def __init__(self): ...
+
+    def build_model(self, num_classes):
+        model = models.efficientnet_b0(weights='IMAGENET1K_V1')
+        model.classifier[1] = nn.Linear(
+            model.classifier[1].in_features,
+            num_classes
+        )
+        return model
