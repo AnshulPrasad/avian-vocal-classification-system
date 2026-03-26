0bd04de7a6d3c4aad787c138cba1b52e64af21f9
Anshul Prasad <anshulprasad5440.com>
2026-03-26 17:35:35 +0530

    don't shuffle for test and validation set

--- src/__init__.py
+++ src/__init__.py
@@ -101,7 +101,7 @@ def split_dataset(species_dir, output_dir, splits=(0.7, 0.15, 0.15)):
 
 def dataset(split):
     obj = BirdSoundDataset(f'{SPLIT_DIR}', RAW_DIR, split=split)
-    loader = DataLoader(obj, batch_size=32, shuffle=True, num_workers=4)
+    loader = DataLoader(obj, batch_size=32, shuffle=(split == 'train'), num_workers=4)
     return loader, obj
 
 def model(num_classes):
