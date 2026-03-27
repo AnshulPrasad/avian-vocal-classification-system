46b7e0ff21a37f4d49e5da005d4cf40d125e7d9d
Anshul Prasad <anshulprasad5440.com>
2026-03-27 23:04:52 +0530

    3 channels (RGB) matching ImageNet standards

--- src/dataset.py
+++ src/dataset.py
@@ -29,7 +29,7 @@ class BirdSoundDataset():
         self.transform = transform or transforms.Compose([
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.5], std=[0.5])
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
 
     def build_and_save_index(self, index_path):
